7962ee54a5c7b251b62ab05fe408769a63384aa9
Joseph Lopez <josephlopez@MacBook-Air-de-Joseph.local>
2023-09-03 20:42:32 -0400

    first commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timerProgress: UIProgressView!
+    
+    var countdown = 0
+    var timer: Timer?
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        
+        
+        if timer != nil {
+            timer?.invalidate()
+        }
+        
+        countdownTimer(time: result)
+    }
+    
+    func countdownTimer(time: Int) {
+        countdown = time
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [self] timer in
+            countdown -= 1
+            let progress = Float(countdown) / Float(time)
+            timerProgress.progress = progress
+            if countdown == 0 {
+                timer.invalidate()
+                titleLabel.text = "DONE!"
+            }
+        }
+    }
 }
